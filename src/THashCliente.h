95b508e4e032ed8d6677fc88f9b950aceeb90c59
Abdallah <aabedraba@gmail.com>
2019-12-03 22:27:51 +0100

    Dispersion doble y mitad de insertar

--- src/THashCliente.h
+++ src/THashCliente.h
@@ -10,7 +10,7 @@
 
 class THashCliente {
 public:
-    THashCliente(int tamTabla);
+    THashCliente(int tamTabla, string tipoInsercion);
     virtual ~THashCliente();
     bool insertar(unsigned long clave, string &dni, Cliente &cli);
     bool buscar(unsigned long clave, string &dni, Cliente &*cli);
@@ -19,10 +19,16 @@ public:
 
 private:
     void hash(unsigned long clave, int intento);
+    unsigned int dispersonDoble(string dni, unsigned int hash, int intentos);
 
 private:
     unsigned int _tamTabla;
     unsigned int _numClientes;
+    unsigned int _maxColisiones;
+    unsigned int _colisiones;
+    unsigned int _numInserciones;
+    const string _tipoDispersion;
+    vector<Cliente*> *_tabla;
 };
 
 
