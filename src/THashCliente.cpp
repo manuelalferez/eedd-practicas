d8aa4cb5443c569d9963dd11f63b0de8eca3a660
Abdallah <aabedraba@gmail.com>
2019-12-04 09:32:04 +0100

    New implementation

--- src/THashCliente.cpp
+++ src/THashCliente.cpp
@@ -11,25 +11,20 @@ THashCliente::THashCliente(int tamTabla, string tipoInsercion):
 {
 
 }
-
-void THashCliente::hash(unsigned long clave, int intento) {
-
-}
-
 bool THashCliente::borrar(unsigned long clave, string &dni) {
 
 }
 
 bool THashCliente::insertar(Cliente &cli) {
 
-    unsigned int pos = djb2(cli.getDni(), cli);
+    unsigned int pos = djb2(cli.getDni(), std::__cxx11::string());
     if (_tipoDispersion == "doble"){
 
     }
 
 }
 
-unsigned int THashCliente::dispersonDoble(const string dni, unsigned int hash, int intentos) {
+unsigned int THashCliente::dispersonDoble(unsigned int hash, int intentos, const string dni, string modo) {
     unsigned int h1 = hash % 57;
     unsigned int h2 = hash % 23;
     unsigned int pos = (h1 + intentos*h2) % _tamTabla;
@@ -39,14 +34,14 @@ unsigned int THashCliente::dispersonDoble(const string dni, unsigned int hash, i
             return pos;
         } else {
             _colisiones++;
-            dispersonDoble(dni, hash, intentos + 1);
+            dispersonDoble(hash, intentos + 1, dni, std::__cxx11::string());
         }
     } else {
         if (_tabla->at(pos)->getDni() == dni ) {
             if (intentos > _maxColisiones) _maxColisiones = intentos;
             return pos;
         } else
-            dispersonDoble(dni, hash, intentos + 1);
+            dispersonDoble(hash, intentos + 1, dni, std::__cxx11::string());
     }
 }
 
@@ -54,17 +49,17 @@ unsigned int THashCliente::numClientes() {
     return _numClientes;
 }
 
-unsigned int THashCliente::djb2(string dni) {
+unsigned int THashCliente::djb2(string dni, string modo) {
     char cstr[dni.size() + 1];
     strcpy(cstr, dni.c_str());
     unsigned long hash = 5381;
     int c;
     while ((c = *cstr++)) hash = ((hash << 5) + hash) + c;
     if ( _tipoDispersion == "doble" ){
-        return dispersonDoble(dni, hash, 0);
+        return dispersonDoble(hash, 0, dni, std::__cxx11::string());
     }
     else if ( _tipoDispersion == "cuadratica" ){
-        return dispersionCuadratica(dni, hash, 0);
+        return dispersionCuadratica(dni, hash, 0, modo);
     }
 }
 
@@ -85,10 +80,6 @@ unsigned int THashCliente::dispersionCuadratica(const string dni, int hash, int 
         } else
             dispersionCuadratica(dni, hash, intentos + 1);
     }
-}
-
-int THashCliente::dispersionDoble(int hash, int intentos) {
-
 }
 
 THashCliente::~THashCliente() {
