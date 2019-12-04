dddbaf9b65351f51e88219cfe92120c74067440c
manuelalferez <manuelalferezruiz@gmail.com>
2019-12-04 09:49:24 +0100

    feat: Insertar y elimijnar

--- src/THashCliente.cpp
+++ src/THashCliente.cpp
@@ -14,7 +14,9 @@ THashCliente::THashCliente(int tamTabla, string tipoInsercion) :
 bool THashCliente::borrar(unsigned long clave, string &dni) {
     unsigned int pos = djb2(dni, "buscar");
     if (pos != INT_MAX) {
-        delete _tabla.at(pos).second; //TODO no está resulto según implementación
+        delete _tabla.at(pos).second;
+        _tabla.at(pos).first = "disponible";
+        _numClientes--;
         return true;
     }
     return false;
@@ -33,7 +35,10 @@ bool THashCliente::insertar(string &dni, Cliente &cli) {
     unsigned int pos = djb2(cli.getDni(), "buscar");
     if (pos == INT_MAX) {
         pos = djb2(cli.getDni(), "insertar");
-        _tabla.at(pos).firts = cli; //TODO no está resulto según implementación
+        _tabla->at(pos).second = cli;
+        _tabla->at(pos).first = "ocupada";
+        _numInserciones++;
+        _numClientes++;
         return true;
     }
     return false;
@@ -42,25 +47,25 @@ bool THashCliente::insertar(string &dni, Cliente &cli) {
 unsigned int THashCliente::dispersonDoble(const string dni, unsigned int hash, unsigned int intentos, string modo) {
     unsigned int h1 = hash % 57;
     unsigned int h2 = hash % 23;
-    unsigned int pos = (h1 + intentos*h2) % _tamTabla;
-    if (modo == "insertar"){
-        if (_tabla->at(pos).first == DISPONIBLE || _tabla->at(pos).first == VACIO ) {
+    unsigned int pos = (h1 + intentos * h2) % _tamTabla;
+    if (modo == "insertar") {
+        if (_tabla->at(pos).first == "disponible" || _tabla->at(pos).first == "vacio") {
             if (intentos > _maxColisiones) _maxColisiones = intentos;
             return pos;
         } else {
             _colisiones++;
             dispersonDoble(dni, hash, intentos + 1, modo);
         }
-    } else if (modo == "busqueda"){
-        if ( _tabla->at(pos).first == DISPONIBLE)
-            dispersonDoble(dni, hash, intentos+1, modo);
-        else if (_tabla->at(pos).first == OCUPADO){
+    } else if (modo == "busqueda") {
+        if (_tabla->at(pos).first == "disponible")
+            dispersonDoble(dni, hash, intentos + 1, modo);
+        else if (_tabla->at(pos).first == "ocupada") {
             if (_tabla->at(pos).second->getDni() == dni)
                 return pos;
             else
-                dispersonDoble(dni, hash, intentos+1, modo);
+                dispersonDoble(dni, hash, intentos + 1, modo);
         }
-        if (modo == VACIO ) {
+        if (modo == VACIO) {
             return INT_MAX;
         }
     }
@@ -83,22 +88,28 @@ unsigned int THashCliente::djb2(string dni, string modo) {
     }
 }
 
-unsigned int THashCliente::dispersionCuadratica(const string dni, int hash, int intentos) {
+unsigned int THashCliente::dispersionCuadratica(const string dni, int hash, int intentos, string modo) {
     unsigned int pos = (hash + (intentos * intentos)) % _tamTabla;
-    if (dni.empty()) {
-        if (_tabla->at(pos) == NULL) {
+    if (modo == "insertar") {
+        if (_tabla->at(pos).first == "disponible" || _tabla->at(pos).first == "vacio") {
             if (intentos > _maxColisiones) _maxColisiones = intentos;
             return pos;
         } else {
             _colisiones++;
-            dispersionCuadratica(dni, hash, intentos + 1);
+            dispersionCuadratica(dni, hash, intentos + 1, modo);
         }
-    } else {
-        if (_tabla->at(pos)->getDni() == dni) {
-            if (intentos > _maxColisiones) _maxColisiones = intentos;
-            return pos;
-        } else
-            dispersionCuadratica(dni, hash, intentos + 1);
+    } else if (modo == "busqueda") {
+        if (_tabla->at(pos).first == "disponible")
+            dispersionCuadratica(dni, hash, intentos + 1, modo);
+        else if (_tabla->at(pos).first == "ocupada") {
+            if (_tabla->at(pos).second->getDni() == dni)
+                return pos;
+            else
+                dispersionCuadratica(dni, hash, intentos + 1, modo);
+        }
+        if (modo == VACIO) {
+            return INT_MAX;
+        }
     }
 }
 
